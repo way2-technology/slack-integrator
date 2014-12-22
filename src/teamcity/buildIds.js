8037fe69f4ef837c31f1aa32c34f5d293de5e3ea
Darin Sleiter <darin.sleiter@way2.com.br>
2014-12-22 18:24:28 -0200

    Add 3 more drivers.

--- src/teamcity/buildIds.js
+++ src/teamcity/buildIds.js
@@ -6,4 +6,7 @@ module.exports = {
   "Drivers.Nexus": "Drivers_Nexus_Ci",
   "Way2 Abnt": "Drivers_Abnt_Ci",
   "Drivers.E3": "Drivers_E3_Ci",
+  "Drivers.Ansi": "Drivers_Ansi_Ci",
+  "Drivers.Cylec": "Drivers_Cylec_Ci",
+  "Integracao.Wits": "Drivers_Wits_Ci"
 }
