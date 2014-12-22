bca5c4729549bfb40a83bc528aad441f8a35cb9b
Darin Sleiter <darin.sleiter@way2.com.br>
2014-12-22 08:42:01 -0200

    Convert tabs to spaces.

--- src/bitbucket/commit.js
+++ src/bitbucket/commit.js
@@ -5,24 +5,24 @@ var buildIdDictionary = require('../teamcity/buildIds');
 var _ = require('lodash');
 
 module.exports = function (req, res) {
-	var body = JSON.parse(req.body.payload);
-	if (body) {
-		var repository = body.repository;
-		var buildId = buildIdDictionary[repository.name];
-		var branches = _.chain(body.commits)
+  var body = JSON.parse(req.body.payload);
+  if (body) {
+    var repository = body.repository;
+    var buildId = buildIdDictionary[repository.name];
+    var branches = _.chain(body.commits)
                     .map(function(c) {
                         return c.branch;
                     })
                     .uniq()
                     .value();
 
-		if (_.contains(branches, 'default')) {
-			var data = {
-				buildTypeId: buildId,
-				branchName: 'default',
-			};
-			postBuild(data);
-		}
-	}
-	res.end();
+    if (_.contains(branches, 'default')) {
+      var data = {
+        buildTypeId: buildId,
+        branchName: 'default',
+      };
+      postBuild(data);
+    }
+  }
+  res.end();
 };
--- src/teamcity/post.js
+++ src/teamcity/post.js
@@ -3,18 +3,18 @@ var request = require('request');
 var config = require('../utils/config');
 
 module.exports = function (data) {
-	request.post({
-		url: config.tcBuildUrl,
-		body: data,
-		json: true,
-		'auth': {
-			'user': config.tcUser,
-			'pass': config.tcPassword,
-			'sendImmediately': true
-		}
-	}, function (error, response, body) {
-		if (error) {
-			console.log(error);
-		}		
-	});
+  request.post({
+    url: config.tcBuildUrl,
+    body: data,
+    json: true,
+    'auth': {
+      'user': config.tcUser,
+      'pass': config.tcPassword,
+      'sendImmediately': true
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+  });
 };
